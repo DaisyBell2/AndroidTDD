c5dde3549d633bbb9d75eed877753c3eeafc15ad
DaisyBell <alina_borz@mail.ru>
2023-10-28 00:05:30 +0300

    refactoring with OOP task 010

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,34 +1,46 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var deletedTextView = false
+    private var state: State = State.Initial
+
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var textView: TextView
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        deletedTextView = savedInstanceState?.getBoolean(KEY) ?: false
 
-        val root = findViewById<LinearLayout>(R.id.rootLayout)
-        val textView = findViewById<TextView>(R.id.titleTextView)
-        val button = findViewById<CustomButton>(R.id.removeButton)
-
-        if (deletedTextView) root.removeView(textView)
+        linearLayout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.removeButton)
 
         button.setOnClickListener {
-            root.removeView(textView)
-            deletedTextView = true
-            button.isEnabled = false
+            state = State.Removed
+            state.apply(linearLayout, textView, button)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY, deletedTextView)
+        outState.putSerializable(KEY, state)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        state = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            savedInstanceState.getSerializable(KEY, State::class.java) as State
+        } else {
+            savedInstanceState.getSerializable(KEY) as State
+        }
+        state.apply(linearLayout, textView, button)
     }
 
     companion object {
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/State.kt
@@ -0,0 +1,26 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import java.io.Serializable
+
+/**
+ * @author DaisyBell on 27.10.2023
+ */
+interface State : Serializable {
+
+    fun apply(linearLayout: LinearLayout, textView: TextView, button: Button)
+
+    object Initial : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) = Unit
+    }
+
+    object Removed : State {
+        override fun apply(linearLayout: LinearLayout, textView: TextView, button: Button) {
+            linearLayout.removeView(textView)
+            button.isEnabled = false
+        }
+    }
+
+}
