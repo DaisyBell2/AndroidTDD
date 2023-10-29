2a990b669a2213f9be711439d503c46ea3541db3
DaisyBell <alina_borz@mail.ru>
2023-10-29 21:42:44 +0300

    done task 014

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,44 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var scope: CoroutineScope
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
+
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        val button = findViewById<Button>(R.id.actionButton)
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+
+            scope.launch(Dispatchers.IO) {
+                delay(3500)
+                withContext(Dispatchers.Main) {
+                    textView.visibility = View.VISIBLE
+                    progressBar.visibility = View.INVISIBLE
+                    button.isEnabled = true
+                }
+            }
+        }
+
     }
 }
